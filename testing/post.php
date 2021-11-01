09303bfb848bb1396abeeefc361d9fe7623b5db0
IsaacRK <auxbot4@gmail.com>
2021-11-01 13:58:08 +0700

    update011121

--- testing/post.php
+++ testing/post.php
@@ -1,118 +1,83 @@
-<?php
-include "../backend/conn.php";
+	<?php
+	include "../backend/conn.php";
+	$sqlHistory = "select * from history";
+	$runHistory = mysqli_query($servConnQuery, $sql);
 
-function xpercent_of_y($x, $y){
-	return $x * ($y / 100);
-}
-function x_aspercent_of_y($x,$y){
-	return (100 * $x) / $y;
-}
-
-
-$elek = '';
-$pera = '';
-$lain = '';
-$totalBarang = '';
-$ei = 0;
-$pi = 0;
-$li = 0;
-
-$sql = "select * from stock";
-$run = mysqli_query($servConnQuery, $sql);
-if(mysqli_num_rows($run)>0){
-	while($row = mysqli_fetch_assoc($run)){
-		//echo 'id: '.$row['stock_id'].' | cate: '.$row['category'].'</br>';
-		if($row['category']=='001'){
-			$elek++;
-			$ei = $ei + $row['amount'];
-		}
-		if($row['category']=='010'){
-			$pera++;
-			$pi = $pi + $row['amount'];
-		}
-		if($row['category']=='100'){
-			$lain++;
-			$li = $li + $row['amount'];
-		}
-		$totalBarang++;
-	}
-}
-
-$totalStorage='';
-$storageCheck='';
-$storageQuery = "select * from penyimpanan";
-$storageRun = mysqli_query($servConnQuery, $storageQuery);
-if(mysqli_num_rows($storageRun)>0){
-	while($srow = mysqli_fetch_assoc($storageRun)){
-		$totalStorage++;
-		if($srow['stock_id']!=null){
-			$storageCheck++;
-		}
-	}
-}
-
-$x = $totalStorage - $storageCheck;
-
-echo 'elektronik = '.$elek;
-echo '</br>peralatan = '.$pera;
-echo '</br>lain-lain = '.$lain;
-echo '</br>total barang = '.$totalBarang;
-echo '</br>-----------------------';
-echo '</br>isi '.$storageCheck.' | total '.$totalStorage;
-
-$e = xpercent_of_y(x_aspercent_of_y($elek,$totalBarang),$storageCheck);
-$p = xpercent_of_y(x_aspercent_of_y($pera,$totalBarang),$storageCheck);
-$l = xpercent_of_y(x_aspercent_of_y($lain,$totalBarang),$storageCheck);
-
-?>
-<html>
-<head>
-<script src="../js/jquery3.6.0.min.js"></script>
-<script src="../js/chart.js"></script>
-</head>
-<body>
-	<canvas id="Chart" style=""></canvas>
-<script>
-
-$(document).ready(function(){
-	var barDisplay = $('#Chart');
-	var nilaiData1 = [<?php echo$elek.','.$pera.','.$lain; ?>];
-	var nilaiData2 = [<?php echo$ei.','.$pi.','.$li; ?>];
-	var warna = ["#1064AE", "#89c5fb"];
 	
-	var barData = {
-		labels: ["Elektronik", "Peralatan", "Lain-lain"],
-		datasets: [{
-			backgroundColor: "#1064AE",
-			data: nilaiData1,
-		}]
-		/*{
-			backgroundColor: "#89c5fb",
-			data: nilaiData2,
-		}]*/
-	};
-	
-	var barOptions = {
-		legend: {
-			display: true,
-			text: "nama text disini"
-		},
-		indexAxis: 'y',
-		scales: {
-			x: {
-				title: {
-					display: true,
-					text: 'Jumlah'
+	//$items = array();
+
+	//while($row = mysqli_fetch_assoc($run)){
+	//	$items[] = $row;
+	//}
+	//$fileName = "aaaa-".date('d-m-Y').".xls";
+	//header('Content-Type: application/vnd.ms-excel');
+	//header('Content-Disposition: attachment; filename='.$fileName);
+	/*
+	if(isset($_POST['report'])){
+		$fileName = "history-aktifitas-".date('d-m-Y').".xls";
+		
+		header('Content-Type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment; filename='.$fileName);
+		
+		$heading = false;
+		
+		if(!empty($items)){
+			foreach($items as $data){
+				if(!$heading){
+					echo implode("\t",array_keys($data))."\n";
+					$heading = true;
 				}
+				echo implode("\t",array_values($data))."\n";
 			}
 		}
-	};
-	
-	var pieChart = new Chart(barDisplay, {
-		type: "bar",
-		data: barData,
-		options: barOptions
-	});
-});
-</script>
-<body>
+		exit();
+	}
+	*/
+	?>
+	<html>
+	<head>
+
+	<style>
+	body{
+		font-family: sans-serif;
+	}
+	table{
+		margin: 20px auto;
+		border-collapse: collapse;
+	}
+	table th,
+	table td{
+		border: 1px solid #3c3c3c;
+		padding: 3px 8px;
+ 
+	}
+	</style>
+
+	</head>
+	<body>
+
+
+-tanggal | nama barang | operator | jumlah barang | untuk apa
+	<table border='1'>
+	<tr>
+	<th>Tanggal</th>
+	<th>Nama Barang</th>
+	<th>Operator</th>
+	<th>Jumlah</th>
+	<th>Status</th>
+	<th>Untuk</th>
+	</tr>
+	<?php foreach($items as $data) { ?>
+	<tr>
+	<td><?php echo $data ['history_id']; ?></td>
+	<td><?php echo $data ['stock_id']; ?></td>
+	<td><?php echo $data ['amount']; ?></td>
+	<td><?php echo $data ['input']; ?></td>
+	<td><?php echo $data ['output']; ?></td>
+	<td><?php echo $data ['user_id']; ?></td>
+	</tr>
+	<?php } ?>
+	</table>
+	</div>
+
+	</body>
