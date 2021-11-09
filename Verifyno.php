ebe43ded4d68b238e1fe7452106178378cfee2af
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-11-09 10:58:23 +0700

    Update Verifyno.php

--- Verifyno.php
+++ Verifyno.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>VERIFY</title>
+<?php include"layout/header.php"?>
+</head>
 <body>
- <p>Please verify your account</p>
+<div class="container mt-5">
+ <div class="card border border-secondary">
+    <div class="card-body text-center">
+    <p>Please verify your account</p>
+    <p>The link should be in your email</p>
+    </div>
+ </div>
+</div>
 </body>
 </html>
