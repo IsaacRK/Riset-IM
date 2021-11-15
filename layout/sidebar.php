40ece0601efd6d6d56e904e04cf582c1ba3dac71
aldibagas <aldibagas982@gmail.com>
2021-11-15 13:51:11 +0700

    Update sidebar.php

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -57,7 +57,7 @@ $Foto_05 = $Foto_03['filename'];
 </svg> Beranda</a>
 <?php
 	if($userlvl != 'supervisor'){
-	echo '<a href="stockinput.php" id="masuk" class="list-group-item-action ripple text-light" aria-current="true"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+	echo '<a href="stockInput.php" id="masuk" class="list-group-item-action ripple text-light" aria-current="true"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
 		<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
 	  </svg> Stok Masuk</a>';
 	}else{}
@@ -90,7 +90,7 @@ $Foto_05 = $Foto_03['filename'];
 	if(urlTxt == "dashboard.php"){
 		document.getElementById("beranda").className = "currentPage";
 	}
-	if(urlTxt == "stockinput.php"){
+	if(urlTxt == "stockInput.php"){
 		document.getElementById("masuk").className = "currentPage";
 	}
 	if(urlTxt == "stockoutput.php"){
