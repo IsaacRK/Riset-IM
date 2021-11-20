e329926d2c7f0a620a44dbbd38105ce1cb115486
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-11-20 13:45:59 +0700

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -99,105 +99,94 @@ if ($userAC == '0'){ ?>
 else { 	include "layout/sidebar.php";
 ?>
 
- <div class="content">
- <div class="container mr-0">
-   <div class="row">
-     <div class="col-1"></div>
-     <div class="col-10">
-       <div class="row mt-5 border card un">
-         <div class="row">
+<div class="content">
+  <div class="container mr-0">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10">
+        <div class="row mt-5 border">
+          <div class="row">
 
-           <div class="col-sm ms-1 text-center mt-2">
-            <form action="backend/upload.php" method="post" enctype="multipart/form-data">
-              <div class="col">              
-               <img src="<?php echo'Photo/'.$Foto_05;?>" class="ikon my-1">
-                <h3 class="text-primary">Ikon Akun</h3>
-              </div>
-            
+            <div class="col-3 ms-1 text-center mt-2">
+             <form action="backend/upload.php" method="post" enctype="multipart/form-data">
+               <div class="col border-0">              
+                 <?php echo "<img src='Photo/$Foto_05' class='ikon my-1'>" ?>
+                 <h3 class="text-primary">Foto akun</h3>
+               </div>
               <div class="row mt-2">
-               <div class="col">
-                 <label for="fileToUpload" class="forl-label">Ganti ikon :</label>
-                  <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" onchange="previewFile(this);">
-               </div>              
-             </div>
-
-             <div class="row mt-2">
                 <div class="col">
-                  <h3 class="text-primary">Ikon Baru</h3></br>
-                  <img id="previewImg" alt="placeholder" class="ikon"></img>
+                  <label for="fileToUpload" class="forl-label">Ganti foto :</label>
+                 	<input class="form-control" type="file" name="fileToUpload" id="fileToUpload" onchange="previewFile(this);">
+                </div>              
+              </div>
+
+              <div class="row mt-2">
+                <div class="col border-0">
+                  <h3 class="text-primary">Foto baru</h3></br>
+                  <img id="previewImg" alt="placeholder" class="ikon my-1"></img>
                 </div>
               </div>
-            
-             <div class="row card border-0 mt-3 mb-3">
-              <div class="col">
-               <input class="btn btn-primary" type="submit" value="Upload Image" name="submit">
-              </div>
-             </div>
 
-            </form>
-           </div>
-
-           <div class="col-sm">
-            <div class="row card border-0 text-center mb-2">
-               <div class="col mb-2">
-                 <h2>Ganti Informasi Akun</h2>
+              <div class="row card border-0 mt-3 mb-3">
+               <div class="col">
+                <input class="btn btn-primary" type="submit" value="Upload Image" name="submit">
                </div>
-            </div>
-            <div class="row border-0">
-             <div class="col">
-               <form action="backend/edit.php" method="post">
+              </div>
 
-                 <div class="row border-0 mb-2">
-                   <div class="col mb-2">
-                     <span class="">Nama : </span></br>
-                     <input class=""  type="text" name="user" id="" placeholder="<?php echo $username;?>"/>
-                   </div>
-                   <div class="col d-flex justify-content-center">
-                     <button class="btn btn-primary mb-3 mt-3" type="submit" name="submit" value="Ganti">Ganti Nama</button>
-                   </div>
-                 </div>
+             </form>
+		      	</div>
 
-                 <div class="row border-0 mb-2">
-                    <div class="col-sm">
-                      <div class="row">
-                  <div class="col-sm mb-2">
-                   <span class="">Kata Sandi : </span></br>
-                   <input class="" style="" type="password" name="pass" id=""/>
-                  </div>
-                  <div class="col-sm mb-2">
-                   <span class="">Konfirmasi Kata Sandi : </span></br>
-                   <input class="" style="" type="password" name="passcon" id=""/>
-                  </div>
-                      </div>
-                    </div>
-                  <div class="col-sm d-flex justify-content-center">
-                   <button class="btn btn-primary my-3 mx-5" type="submit" name="submit" value="register">Ganti Kata Sandi</button>
-                  </div>
-                 </div>
-
-                 <div class="row border-0 mb-2">
-                   <div class="col-sm mb-2">
-                     <span class="">Email :</span></br>
-                     <input class="sfa" type="text" name="email" id="" placeholder="<?php echo $userEM;?>"/>
-                   </div>
-                   <div class="col d-flex justify-content-center">
-                     <button class="btn btn-primary mb-3 mt-3" type="submit" name="submit" value="register">Ganti Email</button>
-                   </div>
-                 </div>
-
-
-               </form>
+            <div class="col-8">
+             <div class="row card border-0 text-center mb-2">
+                <div class="col mb-2">
+			            <h2>Informasi Akun</h2>
+                </div>
              </div>
-           </div>
-           </div>
+             <div class="row border-0">
+              <div class="col">
+                <form action="backend/edit.php" method="post">
+
+                  <div class="row border-0 mb-2">
+                    <div class="col mb-2">
+                      <span class="">Nama : </span></br>
+                      <input class="form-control"  type="text" name="user" id="" placeholder="<?php echo $username;?>"/>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                      <button class="btn btn-primary mb-3 mt-3" type="submit" name="submit" value="Ganti">Ubah</button>
+                    </div>
+                  </div>
+
+                  <div class="row border-0 mb-2">
+                   <div class="col-sm mb-2">
+                    <span class="">Kata sandi : </span></br>
+                    <input class="form-control" style="" type="password" name="pass" id=""/>
+                   </div>
+                   <div class="col-sm mb-2">
+                    <span class="">Konfirmasi kata sandi : </span></br>
+                    <input class="form-control" style="" type="password" name="passcon" id=""/>
+                   </div>
+                  </div>
+
+                  <div class="row border-0 mb-2">
+                    <div class="col-sm mb-2">
+                      <span class="">Email :</span></br>
+                      <input class="form-control" type="text" name="email" id="" placeholder="<?php echo $userEM;?>"/>
+                    </div>
+                  </div>
+
+
+                </form>
+              </div>
+            </div>
+            </div>
 
 
 
-         </div>
-       </div>
-     </div>
-   </div>
- </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php
