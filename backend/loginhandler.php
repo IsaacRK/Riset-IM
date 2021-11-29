7591218755c0d46381d02bb0cc86a3ac58415465
IsaacRK <auxbot4@gmail.com>
2021-11-29 08:20:27 +0700

    patch keamanan
    patch keamanan halaman login

--- backend/loginhandler.php
+++ backend/loginhandler.php
@@ -22,6 +22,25 @@ if(isset($_POST["submit"])){
 	}
 	*/
 	
+	$query = "select id from pengguna where user = ? and pass = ?";
+	
+	$stmt = $servConnQuery->prepare($query);
+	$stmt->bind_param("ss", $user, $pass);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$id = $result->fetch_assoc();
+	if($result->num_rows > 0){
+		session_start();
+		$_SESSION['uid'] = $id['id'];
+		header('location:dashboard.php');
+	}else{
+		echo "
+		<div style='width:100%;pading:5px;background-color:red;color:white;text-align:center;font-weight:bold;'>
+			username atau password salah
+		</div>
+		";
+	}
+	
 }
 
 ?>
