5bebd4b47c649815a3ebbf14e23c2d53f5744cc2
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-12-01 08:30:26 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -31,17 +31,10 @@ if ($userAC == '0'){
 </head>
 
 <body>
-<div id="wrapper">
-   <div class="overlay"></div>
 <?php
-	include"layout/sidebar.php";
+	include"layout/sidebar-old.php";
 ?>
-        <div id="page-content-wrapper">
-            <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
-                <span class="hamb-top"></span>
-          <span class="hamb-middle"></span>
-        <span class="hamb-bottom"></span>
-            </button>
+       
 <div class="content">
 <div class="container mr-0">
 
@@ -201,8 +194,7 @@ if ($userAC == '0'){
     </div>
   </div>
 </div>
-				</div>
-				</div>
+
 
 <?php
 	include"layout/js.php";
