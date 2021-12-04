a6f49afef2dd5c3bf599793e379cf91f8ab24352
IsaacRK <auxbot4@gmail.com>
2021-12-04 14:19:21 +0700

    patch

--- backend/pmbln.php
+++ backend/pmbln.php
@@ -18,26 +18,32 @@ if(isset($_POST['submit'])){
     }else if($jumlah == null){
         header('location:Pembelian.php');
     }else{
-    $stid = "SELECT * FROM stock WHERE stock_name = '$BRNG'";
+    echo$stid = "SELECT * FROM stock WHERE stock_name = '$BRNG'";
     $runsrch = mysqli_query($servConnQuery, $stid);
     $SUS = mysqli_fetch_assoc($runsrch);
     $stckid = $SUS['stock_id'];
 
-    $pmbln = "INSERT into pembelian (id, stock_id, link, jumlah, harga, Ongkir, totalhrg, RAB) 
-    value (default,'.$stckid.','$link','.$jumlah.','.$harga.','.$ONG.','0' ,default)";
+	echo'</br>';
+
+    echo$pmbln = "INSERT into pembelian (id, stock_id, link, jumlah, harga, Ongkir, totalhrg, RAB) 
+    value (default,'$stckid','$link','$jumlah','$harga','$ONG','0' ,default)";
     $inpmb = mysqli_query($servConnQuery, $pmbln);
 
-    $total = "SELECT jumlah * harga + Ongkir from pembelian where stock_id = '$stckid'";
+	echo'</br>';
+
+    echo$total = "SELECT jumlah * harga + Ongkir from pembelian where stock_id = '$stckid'";
     $run = mysqli_query($servConnQuery, $total);
     $row = mysqli_fetch_assoc($run);
     $jumTot = $row['jumlah * harga + Ongkir'];
     $hrgttl = "UPDATE pembelian SET totalhrg = '$jumTot' WHERE stock_id = '$stckid'";
     $frttl = mysqli_query($servConnQuery, $hrgttl);
-
-    $hrg = "UPDATE harga SET beli='$harga' WHERE stock_id = '$stckid'";
+	
+	echo'</br>';
+	
+    echo$hrg = "UPDATE harga SET beli='$harga' WHERE stock_id = '$stckid'";
     $inhr= mysqli_query($servConnQuery, $hrg);
 
-    header('location:Pembelian-work.php');
+    //header('location:Pembelian-work.php');
     }
 
 }
