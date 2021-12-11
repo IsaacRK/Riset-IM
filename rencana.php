3d6d2acaef15182e5e7822cfd2e1a7a7abd132c8
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-12-11 11:24:23 +0700

    Update rencana.php

--- rencana.php
+++ rencana.php
@@ -243,7 +243,42 @@ if($userlvl == 'staff'){
 		</div>
 	
 	</div>
-			
+	
+	<div class="row my-0 mt-0 mb-3">
+		<div class="card shadow-sm px-0">
+			<div class="card-body">
+				<h3>Prediksi</h3>
+
+				<div class="w-100">
+					<form id="searchChart" action="">
+						<div class="row">
+							<div class="col-8">
+								<input class="form-control" name="graphSearch" type="text" id="graphSearch" placeholder="Cari aktivitas barang">
+							</div>
+							<div class="col-4 d-grid gap-2">
+								<button class="btn btn-primary" type="submit" name="graphBtnSearch" value="Cari"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+								<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+								</svg></button>
+							</div>
+						</div>
+					</form>
+				</div>
+				
+				</br>
+
+				<div class="card">
+					<div class="card-body" id="divChart" style="">
+						<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+						
+						
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
 </div>
 </div>
 </div>
@@ -283,6 +318,26 @@ $(function(){
 		}
 	});
 	$('.dataTables_length').addClass('bs-select');
+});
+	
+$(document).ready(function(){
+		$("#divChart").load('layout/a.php');
+});
+$(function(){
+		$("#searchChart").on("submit", function(e){
+			var dataString = $(this).serialize();
+			//alert(dataString);
+			
+			$.ajax({
+				type: "POST",
+				url: "backend/inputhandler.php",
+				data: dataString,
+				success: function(){
+					$("#divChart").load('layout/a.php?'+dataString);
+				}
+			});
+			e.preventDefault();
+		});
 });
 </script>
 
