2c20e34dd33ab102e4204457aa76009deee7834d
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-12-23 11:15:44 +0700

    Memberi komentar

--- backend/verify.php
+++ backend/verify.php
@@ -8,13 +8,19 @@ require_once 'conn.php';
 </head>
 <body>
         <?php
+            //Mendeteksi apakah pengguna masuk melalui surel
             if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+             
+                //Mengambil email dan hash dari alamat web
                 $email = $servConnQuery -> real_escape_string($_GET['email']); 
                 $hash = $servConnQuery -> real_escape_string($_GET['hash']);
+             
+                //Memeriksa apakah ada akun yang memiliki email dan hash yang sama
                 $search = "SELECT email, hash, active FROM pengguna WHERE email='".$email."' AND hash='".$hash."' AND active='0'";
                 $svf = mysqli_query($servConnQuery, $search); 
                 $match  = mysqli_num_rows($svf);
                 if($match > 0){
+                    //Mengatifkan akun
                     $update = "UPDATE pengguna SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'";
                     $running = mysqli_query($servConnQuery, $update);
                     header("Location:../Dashboard.php");
