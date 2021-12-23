77997d928138fbf2f5415e88d57bb3569e37c403
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-12-23 12:03:06 +0700

    Memberi komentar

--- backend/edit.php
+++ backend/edit.php
@@ -4,15 +4,18 @@ require 'usersession.php';
 
 
  if(isset($_POST['submit'])){
+    //Mengambil data-data dari form
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     $email = $_POST['email'];
     $confPass = $_POST['passcon'];
-    
+
+    //Mengambil data pengguna berdasarkan ID	 
     $fetchSql = "select * from pengguna where id=$userId";
     $fetchRun = mysqli_query($servConnQuery, $fetchSql);
     $fetch = mysqli_fetch_assoc($fetchRun);
  if($fetch > 0){   
+    //Melihat apakah kolom Nama Pengguna terisi	 
     if($user==null){
 
     }else{
@@ -20,10 +23,12 @@ require 'usersession.php';
         $change = mysqli_query($servConnQuery, $sql);
         header('location:../Profile.php');
     }
+    //Melihat apakah kolom Kata Sandi terisi	 
     if($pass==null){
 
     }else{
-  if($pass !== $confPass){
+     //Melihat apakah Kata Sandi sama dengan Konfirmasi Kata Sandi	    
+     if($pass !== $confPass){
 		echo "
 		<div style='width:100%;pading:5px;background-color:red;color:white;text-align:center;font-weight:bold;'>
 			Password dan Konfirmasi Password berbeda
@@ -35,6 +40,7 @@ require 'usersession.php';
         header('location:../Profile.php');
     }       
     }
+    //Melihat apakah kolom Email terisi	 
     if($email==null){
     } else{
         $sql = "UPDATE pengguna SET email='$email' WHERE id = '$userId'";
