ae8be8c121edcfb1dd8481d3e3b2bd477900c1e5
ahsanulhd <91927258+ahsanulhd@users.noreply.github.com>
2021-12-23 10:36:20 +0700

    Memberi komentar

--- backend/upload.php
+++ backend/upload.php
@@ -1,13 +1,18 @@
 <?php
 require 'conn.php';
 require 'usersession.php';
+
+//Mengambil ID akun pengguna
 $Id_Prof = $_SESSION["uid"];   
 $user_prof = $username;
+
+//Menunjuk directory
 $target_dir = "../Photo/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+
+//Melihat apakah file gambar gambar asli atau palsu
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
@@ -20,54 +25,59 @@ if(isset($_POST["submit"])) {
   }
 }
 
+//Melihat apakah file berformat JPG, JPEG, PNG atau GIF
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
-// Check if $uploadOk is set to 0 by an error
+//Melihat apakah $uploadOk bernilai 0 dari sebuah error sebelumnya
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-  // if everything is ok, try to upload file
+  
+  //Mengupload file apabila $uploadOk tetap bernilai 1
   } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+      
+      //Mengambil file dan nama dari form
       $filename = $_FILES["fileToUpload"]["name"];
       
-      
+      //Melihat apakah sudah ada gambar yang diasosiasikan dengan akun pengguna      
       $SRCID_01 = "select * from image where user_id = '$Id_Prof'";
       $SRCID_02 = mysqli_query($servConnQuery, $SRCID_01);
       $SRCID_03 = mysqli_num_rows($SRCID_02);
       $SRCID_04 = mysqli_fetch_assoc($SRCID_02);
+      
+      //Apabila tidak ada      
       if ($SRCID_03 == ""){
 
-        // Get all the submitted data from the form
+        //Mengambil semua data yang diperlukan
         $sql = "INSERT INTO image (user_id, username, filename) VALUES ('$Id_Prof', '$user_prof', '$filename')";
-  
-        // Execute query
         mysqli_query($servConnQuery, $sql);
 
+       //Melihat apakah user telah melakukan verifikasi        
        if ($userAC == '0'){
         header("Location:../Verifyno.php");
       }else{
         header("Location:../Profile.php");
-      }  
+      }
+        //Apabila ada gambar yang diasosiasikan dengan akun pengguna
       } else {
         $Delete_03 = $SRCID_04['filename'];
         $file_pointer = "../Photo/$Delete_03";
+        
+        //Menghapus nama file di database
         $Delete_01 = "DELETE FROM image WHERE user_id = '$Id_Prof'";
         $Delete_02 = mysqli_query($servConnQuery, $Delete_01);
+        //Menghapus gambar dari file directory  
         if (!unlink($file_pointer)) { 
         }
         else {   
         } 
-      
-
-        // Get all the submitted data from the form
+ 
         $sql = "INSERT INTO image ( user_id, username, filename) VALUES ('$Id_Prof', '$user_prof', '$filename')";
-  
-        // Execute query
         mysqli_query($servConnQuery, $sql);
 
         header("Location:../Profile.php");
